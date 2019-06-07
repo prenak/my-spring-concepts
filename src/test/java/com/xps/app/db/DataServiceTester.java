eebe5b657df7cd838e22f3413c243be0a4a63256
prenak <prenak@outlook.com>
2019-06-07 21:27:35 +0530

    adding tests to firstbranch

---
+++ src/test/java/com/xps/app/db/DataServiceTester.java
@@ -0,0 +1,11 @@
+package com.xps.app.db;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:com/xps/app/my-app-initiator-context.xml"})
+public class DataServiceTester {
+
+}
